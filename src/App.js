49fc45703e7a050de82aaf14564a33070b6a6e75
vicdev <vtavares.tur@gmail.com>
2022-02-28 10:19:40 -0300

    requisito5

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ class App extends React.Component {
     super();
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
+    this.button = this.button.bind(this);
 
     this.state = {
       cardName: '',
@@ -14,7 +16,7 @@ class App extends React.Component {
       cardAttr1: '0',
       cardAttr2: '0',
       cardAttr3: '0',
-      cardImage: '',
+      cardImage: 'url-to-image',
       cardRare: '',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
@@ -28,7 +30,7 @@ class App extends React.Component {
       cardAttr1: '0',
       cardAttr2: '0',
       cardAttr3: '0',
-      cardImage: '',
+      cardImage: 'url-to-image',
       cardRare: '',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
@@ -40,7 +42,37 @@ class App extends React.Component {
     const valor = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
       [name]: valor,
+    }, () => this.button(valor));
+  }
+
+  button(value) {
+    const { cardAttr1, cardAttr2, cardAttr3, isSaveButtonDisabled } = this.state;
+    const noventa = 90;
+    const cardAtt1 = parseInt(cardAttr1, 10);
+    const cardAtt2 = parseInt(cardAttr2, 10);
+    const cardAtt3 = parseInt(cardAttr3, 10);
+    const duz = 210;
+    const zero = 0;
+    const p = this.setState({
+      isSaveButtonDisabled: true,
     });
+    if (value.length === 0 && isSaveButtonDisabled === false) {
+      this.setState({
+        isSaveButtonDisabled: true,
+      });
+    } else if ((cardAtt1 > noventa) || (cardAttr1 < zero)) {
+      return p;
+    } else if ((cardAtt2 > noventa) || (cardAtt2 < zero)) {
+      return p;
+    } else if ((cardAtt3 > noventa) || (cardAtt3 < zero)) {
+      return p;
+    } else if ((cardAtt1 + cardAtt2 + cardAtt3) > duz) {
+      return p;
+    } else {
+      this.setState({
+        isSaveButtonDisabled: false,
+      });
+    }
   }
 
   render() {
